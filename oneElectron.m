0a35a58a3a3687ca3bc071ad279b3edab46ee212
Rodolphe2021 <94888093+Rodolphe2021@users.noreply.github.com>
2022-02-21 00:05:23 -0500

    Update oneElectron.m

--- oneElectron.m
+++ oneElectron.m
@@ -1,56 +1,71 @@
 set(0,'DefaultFigureWindowStyle','docked')
-set(0,'Defaultaxesfontsize',20)
-set(0,'Defaultaxesfontname','Times New Roman')
-set(0,'Defaultlinelinewidth',2)
+set(0, 'defaultaxesfontsize', 20)
+set(0, 'defaultaxesfontname', 'Times New Roman')
+set(0, 'DefaultLineLineWidth',2);
 
 clear all
 close all
 
-x = 0;
-xt = 0;
-v = 0;
-t = 0;
-f = 1;
-m = 1;
+x = 0; % electron x position
+v = 0; % electron velocity
+t = 0; % time elapsed
 
-dt = 1;
-nt = 1000;
+F = 1;               
+m = 9.10938215e-31;  
+%m=1;
+np = 1;              
+dt = 1;              
+steps = 100;
+tau = 10;            
 
+% multiple particles
 v = zeros(np,1);
 x = zeros(np,1);
-re = 0;
-% re = -1;
 
-for i = 2:nt
-t(i) = t(i-1)+dt;
-v(:,1)= v(:,i-1)+ F/m+dt;
-x(:,i)= x(:,i-1)+ v(:,i-1)*dt+ F/m+dt^2/2;
+re = 0; 
 
-r = rand(np,1) < 0.5;
-v(r,i) = re+v(r,i);
-averageV(i,:) = mean(v,2);
+for i = 2:steps
+    t(i) = t(i-1) + dt;
+    v(:,i) = v(:, i-1) + F/m*dt;
+    x(:,i) = x(:, i-1) + v(:, i-1)*dt + F/m*dt^2/2;
+    
+    if np == 1
+        p = 0.05;
+    else
+        p = 1-exp(dt/tau);
+    end
+    
+    r = rand(np,1) < p;
+    v(r,i) = re*v(r,i);
+    average_v(i,:) = mean(v,2)
+    
+    subplot(3,1,1)
+    plot(t,v,'-');
+    hold on
+    subplot(3,1,1)
+    plot(t,average_v,'g*');
+    hold off
+    xlabel('time')
+    ylabel('avg velocity')
+    title(['Average Drift Velocity:' num2str(average_v(i))])
+    
+    subplot(3,1,2)
+    plot(x(1,:),v(1,:),'r-');
+    hold on
+    subplot(3,1,2)
+    if np == 1
+        plot(x(1,:),average_v(:,1),'g*');
+    end
+    hold off
+    xlabel('x (position)')
+    ylabel('avg velocity')    
 
-subplot(3,1,1),plot(t,v,' ');hold on
-subplot(3,1,1),plot(t,averageV,'g');
-hold off
-
-xlabel('time')
-ylabel('v')
-title(('Average v:' num2str(averageV(i))))
-subplot(3,1,2),plot(x(1,:),v(1,:),'r');
-
-subplot(3,1,2),plot(x(1,:),averageV,'g');
-hold off
-xlabel('x')
-ylabel('v')
-
-subplot(3,1,3),plot(t,x,' ');
-xlabel('time')
-ylabel('x')
-
-pause(0.01)
-
-
+    subplot(3,1,3)
+    plot(t,x,'-');
+    xlabel('time')
+    ylabel('x (position)')
+    
+    pause(0.01)
 end
 
 display('Average V')
